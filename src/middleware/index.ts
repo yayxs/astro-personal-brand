8b547465a94ee29219cb212da457d72cc78b812d
yayxs <leaderywl@163.com>
2024-05-20 09:46:29 +0800

    docs(ai): add ai doc

--- src/middleware/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from 'astro:middleware'
-export const onRequest = defineMiddleware(async (context, next) => {
-  console.log('2222222222')
-  const response = await next()
-  response.headers.set('Content-Type', 'text/html; charset=utf-8')
-
-  return response
-})
